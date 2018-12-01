8ef4c4693cfbd6d39d3e5515925d2f5fb6999dd5
Felipe Gasper <felipe@felipegasper.com>
2018-11-30 23:30:36 -0500

    fix load

--- lib/Protocol/DBus/MsgHdr.pm
+++ lib/Protocol/DBus/MsgHdr.pm
@@ -3,28 +3,9 @@ package Protocol::DBus::MsgHdr;
 use strict;
 use warnings;
 
-sub _load {
-    require Socket;
-
-    require Socket::MsgHdr;
-    if (!Socket::MsgHdr->can('buf')) {
-        die 'Socket::MsgHdr is broken unless loaded at compile time. Please harass Socket::MsgHdr’s maintainer to fix this: https://rt.cpan.org/Public/Bug/Display.html?id=127115';
-    }
-
-    return;
-}
-
-sub load_for_authn {
-    eval { _load() } or do {
-        die "Socket::MsgHdr is required to authenticate because this process’s real and effective credentials do not match. Socket::MsgHdr failed to load, though: $@";
-    };
-    $@ = $dollar_at;
-
-    return;
-}
-
 sub load_for_fds {
-    eval { _load() } or do {
+    my $dollar_at = $@;
+    eval { require Socket::MsgHdr } or do {
         die "Socket::MsgHdr is required to send/receive filehandles but failed to load: $@";
     };
     $@ = $dollar_at;
